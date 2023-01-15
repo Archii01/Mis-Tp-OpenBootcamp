da3bffd57d8bfd2c84106d9cf102ce704ee311de
Archii01 <giancarlozanarini11@gmail.com>
2023-01-15 11:08:25 -0300

    Ejercicio 1 Set

---
+++ Set1.js
@@ -0,0 +1,12 @@
+const familia = ["Manolo", "Pepe", "Martina", "Fracesco"]
+
+const miSet = new Set(familia)
+
+console.log(miSet);
+
+miSet.add("Manolo")
+
+console.log(miSet);
+
+miSet.add("JavaScript")
+console.log(miSet);
